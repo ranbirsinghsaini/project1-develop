5de7da4171817d214c873c4b7416b47a04e18a2c
ranbirsinghsaini <144189054+ranbirsinghsaini@users.noreply.github.com>
2023-12-11 00:39:33 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io
-              systemctl start docker
-              systemctl enable docker
-              sudo docker run hello-world
-              echo "Hello World from \$(hostname -f)" > /var/www/html/index.html
+apt-get update
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
+
+# Pull a predefined Docker image
+docker pull ranbir18/my-class-activity:V2
+
+# Example: Run the pulled Docker image
+# docker run -d -p 80:80 ranbir18/my-class-activity:V2
+
+echo "Hello World from \$(hostname -f)" > /var/www/html/index.html
