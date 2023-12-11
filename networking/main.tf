84273ae5d7e31340173a1c1006817ac07026217f
ranbirsinghsaini <144189054+ranbirsinghsaini@users.noreply.github.com>
2023-12-11 01:50:22 -0500

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -60,12 +60,26 @@ resource "aws_route" "default_public_route" {
 
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
-  description = "Allow all inbound HTTP traffic"
+  description = "Allow all inbound HTTP, HTTPS, and SSH traffic"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
