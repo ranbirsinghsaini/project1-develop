c8d32aa671c5539a38d9fe4bd7d176d003093f75
ranbirsinghsaini <144189054+ranbirsinghsaini@users.noreply.github.com>
2023-12-11 00:32:44 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -3,4 +3,5 @@
               apt-get install -y docker.io
               systemctl start docker
               systemctl enable docker
+              sudo docker run hello-world
               echo "Hello World from \$(hostname -f)" > /var/www/html/index.html
